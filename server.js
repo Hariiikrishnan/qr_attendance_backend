e7ae51cc4fde47ea8242880977923fe663a0f569
hariiikrishnan <therihari36@gmail.com>
2026-02-03 21:51:44 +0530

    Before launch CORS fix

--- server.js
+++ server.js
@@ -12,6 +12,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 // Connect DB
 connectDB();
